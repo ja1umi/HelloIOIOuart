d4e84e9729255df866cbbefd12d9e10c2f74667d
Tsuyoshi GEMMA <tgem-mnihombashi6233@spamex.com>
2011-12-18 21:23:39 +0900

    Bugfix
    1) Bugfix in displayMesssage()
    2) Add more test codes for SC-55mk2

--- HelloIOIOuart/src/my/subject/HelloIOIOuart/HelloIOIOuartActivity.java
+++ HelloIOIOuart/src/my/subject/HelloIOIOuart/HelloIOIOuartActivity.java
@@ -29,7 +29,7 @@ public class HelloIOIOuartActivity extends AbstractIOIOActivity {
 	private ToggleButton button_;
 	private final String Str = "Hello, I'm VIFAM...OK VIFAM, Your number is 7.\n";
 //	private final String Str2 = "\nWipe away all your tears. Together we will conquer fear!\n";
-//	private final String msg = "IOIO meets MIDI.";
+	private final String msg = "IOIO meets MIDI.";
 
 	/**
 	 * Called when the activity is first created. Here we normally initialize
@@ -50,10 +50,11 @@ public class HelloIOIOuartActivity extends AbstractIOIOActivity {
 	 * be called repetitively until the IOIO gets disconnected.
 	 */
 	class IOIOThread extends AbstractIOIOActivity.IOIOThread {
-		private static final int PIN_TX = 14;		// it should be 5V tolerant
+		private static final int PIN_TX = 14;		// it should be 5V tolerant, if necessary
 		private static final int PIN_SERCON_TX = 39;
+		private static final int PIN_EXTLED = 15;	// used as GPIO
 		/** The on-board LED. */
-		private DigitalOutput led_;
+		private DigitalOutput led_1, led_2;
 		private Uart uart_1, uart_2;
 		private OutputStream out, scon;
 
@@ -68,7 +69,8 @@ public class HelloIOIOuartActivity extends AbstractIOIOActivity {
 		 */
 		@Override
 		protected void setup() throws ConnectionLostException {
-			led_ = ioio_.openDigitalOutput(IOIO.LED_PIN, true);
+			led_1 = ioio_.openDigitalOutput(IOIO.LED_PIN, true);
+			led_2 = ioio_.openDigitalOutput(PIN_EXTLED, true);
 //    		uart_1 = ioio_.openUart(null, new DigitalOutput.Spec(PIN_TX, DigitalOutput.Spec.Mode.OPEN_DRAIN), 31250, Uart.Parity.NONE, Uart.StopBits.ONE);
     		uart_1 = ioio_.openUart(IOIO.INVALID_PIN, PIN_TX, 31250, Uart.Parity.NONE, Uart.StopBits.ONE);
     		out = uart_1.getOutputStream();
@@ -84,8 +86,8 @@ public class HelloIOIOuartActivity extends AbstractIOIOActivity {
 //			sendMIDImsg(0x90, 90, 127);	// note on
 //			//sendMIDImsg(0x80, 90, 127);	// note off
 //
-//    		initSoundModule();
-//			displayMessage(msg);
+    		initSoundModule();
+			displayMessage(msg);
 
 		}
 
@@ -99,11 +101,12 @@ public class HelloIOIOuartActivity extends AbstractIOIOActivity {
 		 */
 		@Override
 		protected void loop() throws ConnectionLostException {
-			led_.write(!button_.isChecked());
-			try {
-				sleep(10);
-			} catch (InterruptedException e) {
-			}
+			led_1.write(!button_.isChecked()); delay(10);
+			led_2.write(!button_.isChecked()); delay(10);
+//			try {
+//				sleep(10);
+//			} catch (InterruptedException e) {
+//			}
 			
     		try {
 				scon.write( Str.getBytes() );
@@ -136,15 +139,15 @@ public class HelloIOIOuartActivity extends AbstractIOIOActivity {
 			serOut(data2);
 		}
 		
-//		private void sendMIDImsg2(int data1, int data2) {
-//			serOut(data1);
-//			serOut(data2);
-//		}
+		private void sendMIDImsg2(int data1, int data2) {
+			serOut(data1);
+			serOut(data2);
+		}
 		
-//		private void vsendMIDImsg(int... args) {
-//			for (int i : args)
-//				serOut(i);
-//		}
+		private void vsendMIDImsg(int... args) {
+			for (int i : args)
+				serOut(i);
+		}
 		
 		private void delay(int wait) {
 			try {
@@ -156,30 +159,33 @@ public class HelloIOIOuartActivity extends AbstractIOIOActivity {
 			}			
 		}
 		
-//		private void initSoundModule() {
-//			vsendMIDImsg(0xF0, 0x41, 0x10, 0x42, 0x12, 0x40, 0, 0x7F, 0 ,0x41, 0xF7);	// GS reset
-//			delay(100);
+		private void initSoundModule() {
+			vsendMIDImsg(0xF0, 0x41, 0x10, 0x42, 0x12, 0x40, 0, 0x7F, 0 ,0x41, 0xF7);	// GS reset
+			delay(100);
 //			sendMIDImsg(0xB0, 0, 8);		// "Sine-wave" @ SC-55mk2
 //			sendMIDImsg(0xB0, 0x20, 0);
 //			sendMIDImsg2(0xC0, 80);
-//		}
+			sendMIDImsg(0xB0, 0, 0);		// "flute" @ SC-55mk2
+			sendMIDImsg(0xB0, 0x20, 0);
+			sendMIDImsg2(0xC0, 73);
+		}
 		
-//		protected void displayMessage(String msg) {
-//			int csum, b, dat, b1 = 0x10, b2 = 0, b3 = 0;
-//			String str;
-//			
-//			str = msg.concat("                                ").substring(0, 31);
-//			vsendMIDImsg(0xF0, 0x41, 0x10, 0x45, 0x12, b1, b2, b3);
-//			dat = 0;
-//			for (int i = 0; i <32; i++) {
-//				b = str.charAt(i);
-//				serOut(b);
-//			    dat += b;  
-//			}
-//			csum = 128 - ((b1 + b2 + b3 + dat) & 0x7F);
-//			sendMIDImsg2(csum, 0xF7);
-//			delay(50);
-//		}
+		protected void displayMessage(String msg) {
+			int csum, b, dat, b1 = 0x10, b2 = 0, b3 = 0;
+			String str;
+			
+			str = msg.concat("                                ").substring(0, 32);
+			vsendMIDImsg(0xF0, 0x41, 0x10, 0x45, 0x12, b1, b2, b3);
+			dat = 0;
+			for (int i = 0; i <32; i++) {
+				b = str.charAt(i);
+				serOut(b);
+			    dat += b;  
+			}
+			csum = 128 - ((b1 + b2 + b3 + dat) & 0x7F);
+			sendMIDImsg2(csum, 0xF7);
+			delay(50);
+		}
 
 				
 	} // end of class IOIOThread
